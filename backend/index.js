fcee6e6fc4ef0122baed7ad6e08a123a38aab835
Ritesh Singh <89647516+ritzzh@users.noreply.github.com>
2024-07-01 10:19:14 +0530

    Update index.js
    update Cors

--- backend/index.js
+++ backend/index.js
@@ -31,7 +31,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://book-rack-root-frontend.onrender.com",
     methods: ["GET", "POST"],
   },
 });
